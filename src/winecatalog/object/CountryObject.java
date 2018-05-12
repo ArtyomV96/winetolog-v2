76a8048f3f3bad57b199fe07d609ad186fafd5e8
ArtyomV96 <wans80@inbox.ru>
2018-05-12 20:45:29 +0300

    commit all

---
+++ src/winecatalog/object/CountryObject.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package winecatalog.object;
+
+/**
+ *
+ * @author wans8
+ */
+public class CountryObject {
+    int id;
+    String country;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return country;
+    }
+
+    public void setName(String name) {
+        this.country = name;
+    }
+    
+    
+    
+}
---
+++ src/winecatalog/object/WineObject.java
@@ -0,0 +1,123 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package winecatalog.object;
+
+/**
+ *
+ * @author wans8
+ */
+public class WineObject { 
+    int id;
+    String name;
+    String company;
+    String country;
+    String type;
+    String grape;
+    int year;
+    double vol;
+    double price;
+    String description;
+    String pic;
+    int count;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getGrape() {
+        return grape;
+    }
+
+    public void setGrape(String grape) {
+        this.grape = grape;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public double getVol() {
+        return vol;
+    }
+
+    public void setVol(double vol) {
+        this.vol = vol;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPic() {
+        return pic;
+    }
+
+    public void setPic(String pic) {
+        this.pic = pic;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+    
+    
+}
